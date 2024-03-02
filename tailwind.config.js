03e6ab606f1b737a3d0ec9701016dfc93a7e25f3
Jackson Makinda <me@makindajackson.com>
2024-03-02 11:31:59 +0300

    add: brand color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
       fontFamily: {
         "space-grotesk": ["Space Grotesk", "sans-serif"],
       },
+      colors: {
+        'blue': {
+          '50': '#f2f8fd',
+          '100': '#e3eefb',
+          '200': '#c1ddf6',
+          '300': '#8ac1ef',
+          '400': '#4ca2e4',
+          '500': '#2586d2',
+          '600': '#1769b2',
+          '700': '#145693',
+          '800': '#144878',
+          '900': '#163d64',
+          '950': '#0f2842',
+        },
+      },
     },
   },
   plugins: [],
